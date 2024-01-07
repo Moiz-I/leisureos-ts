65df6d2bc68836caa6749cd690a7a66d76e8ec79
Moiz-I <sky.moizi9@gmail.com>
2024-01-07 14:49:35 +0000

    test aniwave scraper route 2

--- app/scraper.ts
+++ app/scraper.ts
@@ -17,7 +17,11 @@ async function scrapeShowTitles(showName: string): Promise<ShowInfo[]> {
     const response = await fetch(url, {
       headers: {
         "User-Agent":
-          "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
       },
     });
     const body = await response.text();
